0ade2499f77c694e2ea35ad500b45c2d33f593e0
Bautista Madruga <bautistamadruga20@gmail.com>
2021-12-16 23:55:48 -0300

    buttons likes and dislikes added

---
+++ TP-6/static/js/main.js
@@ -0,0 +1,13 @@
+var a = 0;
+function sumar() {
+  a = a + 1;
+  document.getElementById("btn1").value = a;
+  console.log(a)
+
+}
+function restar() {
+  a = a - 1;
+  document.getElementById("btn2").value = a;
+  console.log(a)
+}
+
